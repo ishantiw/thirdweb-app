99695148d93da035ac05243fa673f276515c8b14
Ishan <ishantiw.quasar@gmail.com>
2025-04-09 16:16:05 +0200

    Add thirdweb ecosystem wallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import { ConnectButton } from "thirdweb/react";
 import thirdwebIcon from "./thirdweb.svg";
-import { client } from "./client";
+import { handleLogin, preLogin } from "./wallet";
 
 export function App() {
 	return (
@@ -8,15 +7,7 @@ export function App() {
 			<div className="py-20">
 				<Header />
 
-				<div className="flex justify-center mb-20">
-					<ConnectButton
-						client={client}
-						appMetadata={{
-							name: "Example app",
-							url: "https://example.com",
-						}}
-					/>
-				</div>
+				<EcosystemLogin />
 
 				<ThirdwebResources />
 			</div>
@@ -24,6 +15,131 @@ export function App() {
 	);
 }
 
+function EcosystemLogin() {
+	return (
+		<div>
+			<div className="flex justify-center mb-20">
+				<text className="text-zinc-300 text-base">
+					Login with your email to get started
+				</text>
+			</div>
+			<div className="flex justify-center mb-10" id="preLoginContainer" hidden={false}>
+				<input
+					type="email"
+					placeholder="Enter your email"
+					className="bg-zinc-800 text-zinc-300 px-4 py-2 rounded-lg mr-4"
+					id="email"
+				/>
+
+				<div className="flex items-center ml-4">
+					<input
+						type="checkbox"
+						id="termsCheckbox"
+						className="mr-2"
+					/>
+					<label htmlFor="termsCheckbox" className="text-zinc-300 text-sm">
+						Ecosystem wallet
+					</label>
+				</div>
+
+				<button
+					className="bg-violet-500 text-white px-4 py-2 rounded-lg hover:bg-violet-600 transition-colors"
+					onClick={async () => {
+						const emailInput = document.getElementById("email") as HTMLInputElement | null;
+						const checkbox = document.getElementById("termsCheckbox") as HTMLInputElement | null;
+						const button = document.getElementById("preLoginButton") as HTMLButtonElement | null;
+
+						if (emailInput && checkbox) {
+							console.log("email value ", emailInput.value);
+							const err = await preLogin(emailInput.value, checkbox.checked);
+							if (err && err instanceof Error) {
+								const accountLabel = document.getElementById("accountLabel") as HTMLDivElement | null;
+								if (accountLabel) {
+									accountLabel.hidden = false;
+									accountLabel.innerHTML = `Connected Account: <b>${err.message}</b>`;
+								}
+							}
+							if (button) {
+								button.disabled = true;
+								button.textContent = "Code sent! Please wait...";
+								setTimeout(() => {
+									button.disabled = false;
+									button.textContent = "Get Confirmation Code";
+								}, 30000); // 30 seconds
+							}
+						} else {
+							console.error("Email input or checkbox element not found");
+						}
+					}}
+					id="preLoginButton"
+				>
+					Get Confirmation Code
+				</button>
+			</div>
+
+			<div className="flex justify-center mb-10" id="loginButtonContainer" hidden={false}>
+				<input
+					type="text"
+					placeholder="Enter your verification code"
+					className="bg-zinc-800 text-zinc-300 px-4 py-2 rounded-lg mr-4"
+					id="verificationCode"
+				/>
+				<button
+					className="bg-violet-500 text-white px-4 py-2 rounded-lg hover:bg-violet-600 transition-colors"
+					onClick={async () => {
+						let email: string
+						let verificationCode: string
+						const emailInput = document.getElementById("email") as HTMLInputElement | null;
+						const checkbox = document.getElementById("termsCheckbox") as HTMLInputElement | null;
+						if (emailInput) {
+							email = emailInput.value;
+						} else {
+							console.error("Email input element not found");
+						}
+						const verificationCodeInput = document.getElementById("verificationCode") as HTMLInputElement | null;
+						if (verificationCodeInput) {
+							verificationCode = verificationCodeInput.value;
+						}
+						else {
+							console.error("Verification code input element not found");
+						}
+
+						if (emailInput && verificationCodeInput && checkbox) {
+							console.log("email value ", emailInput.value)
+							console.log("verification code value ", verificationCodeInput.value)
+							try {
+								const account = await handleLogin(
+									emailInput.value,
+									verificationCodeInput.value,
+									checkbox.checked,
+								)
+								console.log("Account connected:", account);
+
+								const accountLabel = document.getElementById("accountLabel") as HTMLDivElement | null;
+								if (accountLabel) {
+									accountLabel.hidden = false;
+									accountLabel.innerHTML = `Connected Account: <b>${account.address}</b>`;
+								}
+							} catch (error: any) {
+								const accountLabel = document.getElementById("accountLabel") as HTMLDivElement | null;
+								if (accountLabel) {
+									accountLabel.hidden = false;
+									accountLabel.innerHTML = `Connected Account: <b>${error.message}</b>`;
+								}
+							}
+
+						}
+					}
+					}
+					id="loginButton"
+				>
+					Login with verification code
+				</button>
+			</div>
+			<div className="flex justify-center mb-10">
+				<label hidden={true} id="accountLabel" className="bg-gray-700 text-white px-4 py-2 rounded-lg"></label>
+			</div></div>)
+}
 function Header() {
 	return (
 		<header className="flex flex-col items-center mb-20 md:mb-20">
---
+++ src/wallet.tsx
@@ -0,0 +1,80 @@
+import {
+    preAuthenticate,
+    ecosystemWallet,
+    inAppWallet,
+    Account,
+} from "thirdweb/wallets";
+import { client } from "./client";
+import { liskSepolia } from "./lisk_network";
+
+export const preLogin = async (email: string, isEcosystemWallet=false): Promise<Error | undefined> => {
+    // send email verification code
+    console.log("isEcosystemWallet:", isEcosystemWallet);
+    console.log("preLogin called with email:", email);
+    console.log("ecosystem id:", import.meta.env.VITE_ECOSYSTEM_ID);
+    console.log("partner id:", import.meta.env.VITE_PARTNER_ID);
+    const ecosystem = {
+        id: import.meta.env.VITE_ECOSYSTEM_ID as `ecosystem.${string}`,
+        partnerId: import.meta.env.VITE_PARTNER_ID,
+    }
+    if (isEcosystemWallet) {
+        console.log("ecosystemWallet is true");
+        await preAuthenticate({
+            client,
+            strategy: "email",
+            email,
+            ecosystem,
+        }).catch((error) => {
+            console.error("Error during pre-authentication:", error);
+            return error
+        }).then(() => {
+            console.log("preAuthenticate completed");
+            return null;
+        });
+    }
+
+    // Normal wallet
+    await preAuthenticate({
+        client,
+        strategy: "email",
+        email,
+    }).catch((error) => {
+        console.error("Error during pre-authentication:", error);
+        return error
+    }).then(() => {
+        console.log("preAuthenticate completed");
+        return null;
+    });
+
+    return
+};
+
+export const handleLogin = async (email: string, verificationCode: string, isEcosystemWallet=false): Promise<Account> => {
+    // verify email and connect
+    if (isEcosystemWallet) {
+        const ecoWallet = ecosystemWallet(import.meta.env.VITE_ECOSYSTEM_ID as `ecosystem.${string}`,
+            {
+                partnerId: import.meta.env.VITE_PARTNER_ID,
+            }
+        );
+        const account = await ecoWallet.connect({
+            client,
+            strategy: "email",
+            email,
+            verificationCode,
+            chain: liskSepolia,
+        });
+        return account;
+    }
+
+    const ecoWallet = inAppWallet();
+    const account = await ecoWallet.connect({
+        client,
+        strategy: "email",
+        email,
+        verificationCode,
+        chain: liskSepolia,
+    });
+    return account;
+
+};
